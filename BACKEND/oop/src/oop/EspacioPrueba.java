a2a9264b91a51bb274d81af8dab69e3663ea50f9
Java <Java@192.168.0.126>
2024-02-15 18:00:13 +0100

    Uso de la herencia con Espacio

--- BACKEND/oop/src/oop/EspacioPrueba.java
+++ BACKEND/oop/src/oop/EspacioPrueba.java
@@ -1,5 +1,8 @@
 package oop;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
 public class EspacioPrueba {
 	public static void main(String[] args) {
 		Espacio e = new Espacio("Bilbao");
@@ -9,6 +12,7 @@ public class EspacioPrueba {
 		e.getPersonas().add(new Persona("Javier"));
 		e.getPersonas().add(new Persona("Pepe"));
 		e.getPersonas().add(new Persona("Juan"));
+		e.getPersonas().add(new Empleado("Javier", "Lete", LocalDate.of(2000, 2, 3), new BigDecimal("23456.78")));
 		
 		System.out.println(e);
 	}
