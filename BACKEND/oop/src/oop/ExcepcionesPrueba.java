a106a614fde35f20c3f64a026fa3cef5cb9357e8
Java <Java@192.168.0.126>
2024-02-20 17:31:48 +0100

    Excepciones

---
+++ BACKEND/oop/src/oop/ExcepcionesPrueba.java
@@ -0,0 +1,31 @@
+package oop;
+
+public class ExcepcionesPrueba {
+
+	public static void main(String[] args) {
+		int div, a = 6, b = 2;
+
+		try {
+			var arr = new int[2];
+			
+			arr[2] = 5;
+			
+			String s = null;
+
+			// System.out.println(s.toUpperCase());
+
+			div = a / b;
+
+			System.out.println(div);
+		} catch (ArithmeticException e) {
+			System.out.println("Error en la división");
+		} catch(NullPointerException e) {
+			System.out.println("Error de null");
+		} finally {
+			System.out.println("Por mis narices");
+		}
+
+		System.out.println("FIN");
+	}
+
+}
