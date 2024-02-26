d6e7b0db9e7007e4a9661afda5b8a3514ed8129c
Java <Java@192.168.0.126>
2024-02-26 17:18:32 +0100

    Creamos una fábrica para crear DAOs, con su propia excepción

---
+++ BACKEND/tienda/src/com/ipartek/formacion/tienda/accesodatos/FabricaDao.java
@@ -0,0 +1,36 @@
+package com.ipartek.formacion.tienda.accesodatos;
+
+import java.io.IOException;
+import java.util.Properties;
+
+public class FabricaDao {
+	private DaoProducto daoProducto = null;
+	
+	public FabricaDao() {
+		Properties props = new Properties();
+		
+		try {
+			props.load(FabricaDao.class.getClassLoader().getResourceAsStream("tienda.properties"));
+			
+			String tipo = props.getProperty("accesodatos.tipo");
+			
+			switch(tipo) {
+			case "treemap":
+				daoProducto = DaoProductoTreeMap.getInstancia();
+				break;
+			case "fichero":
+				// daoProducto = DaoProductoFichero.getInstancia();
+				break;
+			default:
+				throw new AccesoDatosException("No se reconoce el tipo " + tipo);
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public DaoProducto getDaoProducto() {
+		return daoProducto;
+	}
+}
