08f5b35e1e49f7b980e5e5a5fc6a7f7bdd16c615
Java <Java@192.168.0.126>
2024-02-28 16:56:46 +0100

    Gestión de excepciones en JDBC

--- BACKEND/tienda/src/com/ipartek/formacion/jdbc/JdbcPrueba.java
+++ BACKEND/tienda/src/com/ipartek/formacion/jdbc/JdbcPrueba.java
@@ -18,84 +18,77 @@ public class JdbcPrueba {
 	private static final String SQL_UPDATE = "UPDATE productos SET nombre=?, precio=?, fecha_caducidad=? WHERE id=?";
 	private static final String SQL_DELETE = "DELETE FROM productos WHERE id=?";
 
-	public static void main(String[] args) throws SQLException {
-		Connection con = DriverManager.getConnection(URL, USER, PASS);
-		PreparedStatement pst = con.prepareStatement(SQL_SELECT);
-		ResultSet rs = pst.executeQuery();
+	public static void main(String[] args) {
+		try (Connection con = DriverManager.getConnection(URL, USER, PASS);) {
+			try (PreparedStatement pst = con.prepareStatement(SQL_SELECT); ResultSet rs = pst.executeQuery();) {
+				while (rs.next()) {
+					System.out.printf("%20s %20s %20s %20s\n", rs.getLong("id"), rs.getString("nombre"),
+							rs.getBigDecimal("precio"), rs.getString("fecha_caducidad"));
+				}
+			}
 
-		while (rs.next()) {
-			System.out.printf("%20s %20s %20s %20s\n", rs.getLong("id"), rs.getString("nombre"),
-					rs.getBigDecimal("precio"), rs.getString("fecha_caducidad"));
+			try (PreparedStatement pst = con.prepareStatement(SQL_SELECT_ID);) {
+				Long id = 2L;
+
+				pst.setLong(1, id);
+
+				try (ResultSet rs = pst.executeQuery()) {
+					if (rs.next()) {
+						System.out.printf("%20s %20s %20s %20s\n", rs.getLong("id"), rs.getString("nombre"),
+								rs.getBigDecimal("precio"), rs.getString("fecha_caducidad"));
+					} else {
+						System.out.println("No se ha encontrado");
+					}
+				}
+			}
+
+			try (PreparedStatement pst = con.prepareStatement(SQL_INSERT);) {
+
+				String nombre = "Producto Nuevo";
+				BigDecimal precio = new BigDecimal("132435");
+				String fecha = "2026-01-02";
+
+				pst.setString(1, nombre);
+				pst.setBigDecimal(2, precio);
+				pst.setString(3, fecha);
+
+				int numeroRegistrosModificados = pst.executeUpdate();
+
+				System.out.println(numeroRegistrosModificados);
+			}
+
+			try (PreparedStatement pst = con.prepareStatement(SQL_UPDATE);) {
+
+				String nombre = "Modificado";
+				BigDecimal precio = BigDecimal.ONE;
+				String fecha = "2027-01-01";
+
+				Long id = 1L;
+
+				pst.setString(1, nombre);
+				pst.setBigDecimal(2, precio);
+				pst.setString(3, fecha);
+
+				pst.setLong(4, id);
+
+				int numeroRegistrosModificados = pst.executeUpdate();
+
+				System.out.println(numeroRegistrosModificados);
+			}
+
+			Long id = 2L;
+
+			try (PreparedStatement pst = con.prepareStatement(SQL_DELETE);) {
+
+				pst.setLong(1, id);
+
+				int numeroRegistrosModificados = pst.executeUpdate();
+
+				System.out.println(numeroRegistrosModificados);
+			}
+		} catch (SQLException e) {
+			System.out.println("Error en la conexión");
+			e.printStackTrace();
 		}
-
-		rs.close();
-		pst.close();
-
-		pst = con.prepareStatement(SQL_SELECT_ID);
-
-		Long id = 2L;
-
-		pst.setLong(1, id);
-		rs = pst.executeQuery();
-
-		if (rs.next()) {
-			System.out.printf("%20s %20s %20s %20s\n", rs.getLong("id"), rs.getString("nombre"),
-					rs.getBigDecimal("precio"), rs.getString("fecha_caducidad"));
-		} else {
-			System.out.println("No se ha encontrado");
-		}
-
-		rs.close();
-		pst.close();
-
-		pst = con.prepareStatement(SQL_INSERT);
-
-		String nombre = "Producto Nuevo";
-		BigDecimal precio = new BigDecimal("132435");
-		String fecha = "2026-01-02";
-
-		pst.setString(1, nombre);
-		pst.setBigDecimal(2, precio);
-		pst.setString(3, fecha);
-
-		int numeroRegistrosModificados = pst.executeUpdate();
-
-		pst.close();
-
-		System.out.println(numeroRegistrosModificados);
-
-		pst = con.prepareStatement(SQL_UPDATE);
-
-		nombre = "Modificado";
-		precio = BigDecimal.ONE;
-		fecha = "2027-01-01";
-
-		id = 1L;
-
-		pst.setString(1, nombre);
-		pst.setBigDecimal(2, precio);
-		pst.setString(3, fecha);
-
-		pst.setLong(4, id);
-
-		numeroRegistrosModificados = pst.executeUpdate();
-		
-		pst.close();
-		
-		System.out.println(numeroRegistrosModificados);
-		
-		id = 2L;
-		
-		pst = con.prepareStatement(SQL_DELETE);
-		
-		pst.setLong(1, id);
-		
-		numeroRegistrosModificados = pst.executeUpdate();
-		
-		pst.close();
-		
-		System.out.println(numeroRegistrosModificados);
-		
-		con.close();
 	}
 }
