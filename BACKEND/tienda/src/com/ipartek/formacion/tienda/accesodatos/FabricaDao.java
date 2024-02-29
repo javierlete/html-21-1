f23bcaad26cbfb51067d64295b9fa43548433f7d
Java <Java@192.168.0.126>
2024-02-29 17:13:01 +0100

    Versión de acceso a datos con MySQL

--- BACKEND/tienda/src/com/ipartek/formacion/tienda/accesodatos/FabricaDao.java
+++ BACKEND/tienda/src/com/ipartek/formacion/tienda/accesodatos/FabricaDao.java
@@ -13,16 +13,23 @@ public class FabricaDao {
 			props.load(FabricaDao.class.getClassLoader().getResourceAsStream("tienda.properties"));
 			
 			String tipo = props.getProperty("accesodatos.tipo");
+			String baseDeDatos = props.getProperty("accesodatos.bdd");
+			String usuario = props.getProperty("accesodatos.usuario");
+			String password = props.getProperty("accesodatos.password");
+			
 			
 			switch(tipo) {
 			case "treemap":
 				daoProducto = DaoProductoTreeMap.getInstancia();
 				break;
 			case "fichero":
-				daoProducto = new DaoProductoFichero("tienda.dat");
+				daoProducto = new DaoProductoFichero(baseDeDatos);
 				break;
 			case "sqlite":
-				daoProducto = new DaoProductoSqlite("tienda.db");
+				daoProducto = new DaoProductoSqlite(baseDeDatos);
+				break;
+			case "mysql":
+				daoProducto = new DaoProductoMySql(baseDeDatos, usuario, password);
 				break;
 			default:
 				throw new AccesoDatosException("No se reconoce el tipo " + tipo);
