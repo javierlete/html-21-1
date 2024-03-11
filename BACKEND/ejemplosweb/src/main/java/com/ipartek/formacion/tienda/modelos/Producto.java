ba78876401cf95ec421aac857a96140fad74f783
Java <Java@192.168.0.126>
2024-03-11 17:13:36 +0100

    Decoramos las entidades con anotaciones de JPA (Jakarta Persistence API)

--- BACKEND/ejemplosweb/src/main/java/com/ipartek/formacion/tienda/modelos/Producto.java
+++ BACKEND/ejemplosweb/src/main/java/com/ipartek/formacion/tienda/modelos/Producto.java
@@ -4,21 +4,36 @@ import java.io.Serializable;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+// LOMBOK
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
+
+// JPA
+@Entity
+@Table(name = "productos")
 public class Producto implements Serializable {
 	
 	private static final long serialVersionUID = 4833961369800111940L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String nombre;
 	private BigDecimal precio;
+	
+	@Column(name = "fecha_caducidad")
 	private LocalDate fechaCaducidad;
 }
--- BACKEND/ejemplosweb/src/main/java/com/ipartek/formacion/tienda/modelos/Usuario.java
+++ BACKEND/ejemplosweb/src/main/java/com/ipartek/formacion/tienda/modelos/Usuario.java
@@ -1,18 +1,39 @@
 package com.ipartek.formacion.tienda.modelos;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+// LOMBOK
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
+
+// JPA
+@Entity
+@Table(name = "usuarios")
 public class Usuario {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
+	@Column(length = 50)
 	private String email;
+	
+	@Column(length = 50)
 	private String password;
+	
+	@Column(length = 50)
 	private String nombre;
+	
+	@Column(length = 10)
 	private String rol;
 }
