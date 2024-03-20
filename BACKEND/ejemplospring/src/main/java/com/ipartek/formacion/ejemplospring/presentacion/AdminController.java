822d7dfaf5bd256615cc6153f816d7df2dfa34c5
Java <Java@192.168.0.126>
2024-03-20 16:13:08 +0100

    Editar, añadir y guardar

--- BACKEND/ejemplospring/src/main/java/com/ipartek/formacion/ejemplospring/presentacion/AdminController.java
+++ BACKEND/ejemplospring/src/main/java/com/ipartek/formacion/ejemplospring/presentacion/AdminController.java
@@ -4,8 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.ipartek.formacion.ejemplospring.entidades.Producto;
 import com.ipartek.formacion.ejemplospring.servicios.AdminService;
 
 @Controller
@@ -21,5 +24,31 @@ public class AdminController {
 		modelo.addAttribute("productos", productos);
 		
 		return "admin/listado";
+	}
+	
+	@GetMapping("/formulario")
+	public String formulario(Producto producto) {
+		return "admin/formulario";
+	}
+	
+	@GetMapping("/formulario/{id}")
+	public String formularioConProducto(@PathVariable Long id, Model modelo) {
+		var producto = servicio.verDetalleProducto(id);
+		
+		modelo.addAttribute("producto", producto);
+		
+		return "admin/formulario";
+	}
+	
+	@PostMapping("/formulario")
+	public String formularioPost(Producto producto) {
+		
+		if(producto.getId() == null) {
+			servicio.agregarProducto(producto);
+		} else {
+			servicio.modificarProducto(producto);
+		}
+		
+		return "redirect:/admin";
 	}
 }
