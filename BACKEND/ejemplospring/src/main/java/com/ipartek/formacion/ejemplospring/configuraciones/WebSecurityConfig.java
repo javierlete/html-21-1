79b677641927cde12c0517e514a4bc80e42636f0
Java <Java@192.168.0.126>
2024-03-21 16:18:06 +0100

    Autenticación por JDBC con MySQL

--- BACKEND/ejemplospring/src/main/java/com/ipartek/formacion/ejemplospring/configuraciones/WebSecurityConfig.java
+++ BACKEND/ejemplospring/src/main/java/com/ipartek/formacion/ejemplospring/configuraciones/WebSecurityConfig.java
@@ -1,52 +1,57 @@
 package com.ipartek.formacion.ejemplospring.configuraciones;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
 
-	// Autenticaciones
-    @Bean
-    UserDetailsService userDetailsService() {
-		UserDetails javier =
-			 User.withDefaultPasswordEncoder()
-				.username("javier")
-				.password("lete")
-				.roles("ADMIN")
-				.build();
-		UserDetails pepe =
-				User.withDefaultPasswordEncoder()
-				.username("pepe")
-				.password("perez")
-				.roles("USER")
-				.build();
+	@Autowired
+	private DataSource dataSource;
 
-		return new InMemoryUserDetailsManager(javier, pepe);
+	@Bean
+	PasswordEncoder passwordEncoder() {
+		return NoOpPasswordEncoder.getInstance(); // new BCryptPasswordEncoder();
+	}
+	
+	// Autenticaciones
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+		auth.jdbcAuthentication().dataSource(dataSource);
 	}
 
-    // Autorizaciones
+	// Autorizaciones
 	@Bean
 	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-		http
-			.authorizeHttpRequests((requests) -> requests
-				.requestMatchers("/admin/**").hasRole("ADMIN")
-				.anyRequest().permitAll()
-			)
-			.formLogin((form) -> form
-				.loginPage("/login")
-				.permitAll()
-			)
-			.logout((logout) -> logout.permitAll());
-	
+		http.authorizeHttpRequests(
+				(requests) -> requests.requestMatchers("/admin/**").hasRole("ADMIN").anyRequest().permitAll())
+				.formLogin((form) -> form.loginPage("/login").permitAll()).logout((logout) -> logout.permitAll());
+
 		return http.build();
 	}
 }
+
+/*
+create table users(
+username varchar(50) not null primary key,
+password varchar(500) not null,
+enabled boolean not null
+);
+
+create table authorities (
+username varchar(50) not null,
+authority varchar(50) not null,
+constraint fk_authorities_users foreign key(username) references users(username)
+);
+create unique index ix_auth_username on authorities (username,authority);
+*/
