427455985528a206c9b74fca31457d61955f2905
Java <Java@192.168.0.126>
2024-04-02 15:31:39 +0200

    Ipartindek: (Spring Boot DevTools, Spring Web, Thymeleaf)

---
+++ FULLSTACK/ipartindek/src/main/java/com/ipartek/formacion/ipartindek/IpartindekApplication.java
@@ -0,0 +1,13 @@
+package com.ipartek.formacion.ipartindek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IpartindekApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IpartindekApplication.class, args);
+	}
+
+}
---
+++ FULLSTACK/ipartindek/src/test/java/com/ipartek/formacion/ipartindek/IpartindekApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ipartek.formacion.ipartindek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IpartindekApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
